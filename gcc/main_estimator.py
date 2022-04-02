e1ef88d8238a19ca7e1e5bc283e993ebe0ba563c
mahansen <hansen1996@foxmail.com>
2022-04-02 17:23:26 +0800

    bugfix doing

--- gcc/main_estimator.py
+++ gcc/main_estimator.py
@@ -3,7 +3,7 @@ from utils.my_enum import interfaceState
 from utils.record import pktRecord
 from .gcc_twcc_estimator import GCC
 
-InitialBWE = 300 * 1000  # 300 kbps,初始带宽
+InitialBWE = 1000 * 1000  # 1 mbps,初始带宽
 BaseDelay = 200  # ms, 假设传播时延
 
 
