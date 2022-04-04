11556dc237af979b0de25cc9c8cb81058e80f30b
Hansen Ma <hansen1996@foxmail.com>
2022-04-04 17:05:42 +0800

    bugfix version 1.0

--- plot/drawCurve.py
+++ plot/drawCurve.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from more_itertools import chunked
 
 
 class Line:
@@ -13,6 +14,20 @@ def draw(bigName, *data: Line):
 	for line in data:
 		# x = line.x  # report_interval
 		y = line.y  # 
+		name = line.name
+		plt.plot(y, label=name)
+	plt.legend()
+	plt.savefig("./fig/test" + bigName)
+	plt.show()
+	plt.close()
+
+
+def drawWith15Scale(bigName, *data: Line):
+	bigName = bigName
+	for line in data:
+		# x = line.x  # report_interval
+		y = line.y
+		y = [sum(x) / len(x) for x in chunked(y, 15)]
 		name = line.name
 		plt.plot(y, label=name)
 	plt.legend()
