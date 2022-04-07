89f931e6f3523e6ba25a66e97acc79fa0d2376a4
Hansen Ma <hansen1996@foxmail.com>
2022-04-07 11:53:54 +0800

    drl buf fix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import glob
 
+import torch
 from scipy.signal import savgol_filter
 
+from deep_rl.actor_critic import ActorCritic
 from offlineStatTest import writeStatsReports
 from plot.plotTool import Line, drawLine
 from rtc_env import GymEnv
+from rtc_env import log_to_linear
 from utils.trace_analyse import genTraceCap
 from utils.trace_analyse import readTrace, preprocess
 
@@ -20,6 +23,58 @@ def scaleTraceCap(tracePath) -> Line:
 	return traceCap
 
 
+def drlEstimatorTest(tracePath, modelPath):
+	estimationName = "drl"
+	env = GymEnv()
+	env.setAlphaRtcGym(tracePath)
+	traceName, tracePatterns = readTrace(tracePath)
+	
+	model = ActorCritic(5, 1, exploration_param=0.05)
+	model.load_state_dict(torch.load(modelPath))
+	
+	max_step = 100000
+	step = 0
+	stepList = []
+	recvList = []
+	targetRate = []
+	done = False
+	state = torch.Tensor([0.0 for _ in range(5)])
+	while not done and step < max_step:
+		action, _, _ = model.forward(state)
+		state, reward, done, _ = env.step(action)
+		targetRate.append(log_to_linear(action))
+		recvList.append(log_to_linear(state[0]))
+		state = torch.Tensor(state)
+		stepList.append(step)
+		step += 1
+	
+	dirName = "fig"
+	
+	capCurve = scaleTraceCap(tracePath)
+	
+	gccRate = Line()
+	gccRate.name = traceName + "-gccRate" + "-" + estimationName
+	gccRate.x = stepList
+	gccRate.y = [x / 1000000 for x in targetRate]  # mbps
+	gccRate.y = savgol_filter(gccRate.y, 20, 1, mode="nearest")
+	
+	recvRate = Line()
+	recvRate.name = traceName + "-recvRate" + "-" + estimationName
+	recvRate.x = stepList
+	recvRate.y = [x / 1000000 for x in recvList]  # mbps
+	recvRate.y = savgol_filter(recvRate.y, 20, 1, mode="nearest")
+	
+	# delayCurve = Line()
+	# delayCurve.name = traceName + "-delay-" + estimationName
+	# delayCurve.x = stepList
+	# delayCurve.y = delayList
+	# delayCurve.y = savgol_filter(delayCurve.y, 20, 1, mode="nearest")
+	
+	traceCap = genTraceCap(tracePath)
+	
+	drawLine(dirName, traceName + "-rate-" + estimationName, gccRate, recvRate, traceCap)
+
+
 def estimatorTest(tracePath, estimatorTag):
 	if estimatorTag == 0:
 		estimationName = "OwnGCC"
@@ -29,9 +84,8 @@ def estimatorTest(tracePath, estimatorTag):
 		testversion = "2"
 	
 	env = GymEnv()
-	traceName, tracePatterns = readTrace(tracePath)
-	
 	env.setAlphaRtcGym(tracePath)
+	traceName, tracePatterns = readTrace(tracePath)
 	
 	max_step = 100000
 	traceDone = False
